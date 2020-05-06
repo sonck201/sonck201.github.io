abf70c666521fbf3f419085239ff3465212aad63
Iamck <thanhson_it201@yahoo.com>
2020-05-06 22:42:56 +0700

    Updates

---
+++ precache-manifest.a1bb404f7b55aa504fb25b119fdf0eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c65f70df8a4661aaf5aa1f8e8939f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1cdf00bc7790c39616a",
+    "url": "/static/css/main.e6c91ca0.chunk.css"
+  },
+  {
+    "revision": "fecd893ccbd36d9b8022",
+    "url": "/static/js/2.1eecd21e.chunk.js"
+  },
+  {
+    "revision": "ebc5d087fc4c4229c0680561aa363996",
+    "url": "/static/js/2.1eecd21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1cdf00bc7790c39616a",
+    "url": "/static/js/main.bde13ab7.chunk.js"
+  },
+  {
+    "revision": "8b0b2313121fabb41393",
+    "url": "/static/js/runtime-main.90db9c09.js"
+  }
+]);
